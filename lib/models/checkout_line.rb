204fc64e091e620710916ed11439866b24f3c3da
HoangTL <hoangtl2907@gmail.com>
2019-04-08 06:53:24 +0700

    Add logic checkout with promotion

--- lib/models/checkout_line.rb
+++ lib/models/checkout_line.rb
@@ -1,9 +1,11 @@
 class CheckoutLine
-  attr_accessor :item_id, :quantity, :unit_price
-  def initialize(item)
+  attr_accessor :item_id, :quantity, :unit_price, :discount_price
+
+  def initialize(item, promotional_rule = nil)
     @item_id = item.id
     @quantity = 1
     @unit_price = item.price
+    @promotional_rule = promotional_rule
   end
 
   def inc_one_quantity
@@ -11,7 +13,20 @@ class CheckoutLine
   end
 
   def total_price
-    total_price = unit_price * quantity
+    total_price = if is_discounted?
+                    @promotional_rule.total_discount_price self
+                  else
+                    unit_price * quantity
+                  end
+  end
+
+  def has_promotion?
+    !@promotional_rule.nil?
+  end
+
+  def is_discounted?
+    return false if !has_promotion?
+    @promotional_rule.satify_condition? self
   end
 
 end
